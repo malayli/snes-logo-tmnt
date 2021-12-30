5027af4ad8620d1379c57ec814d768b7ce88a67a
Malik Alayli <malik.alayli@gmail.com>
2021-12-30 23:55:41 +0100

    Load Game Logo with x,y positions

--- src/main.c
+++ src/main.c
@@ -5,13 +5,9 @@
 extern char gameLogoSprite, gameLogoSprite_end;
 extern char gameLogoPalette;
 
-int main(void) {
-    // Initialize SNES 
-	consoleInit();
-
-	// Now Put in 256 color mode
-	setMode(BG_MODE3, 0);
-
+void loadGameLogo(u16 xspr, u16 yspr) {
+	oamInit();
+	
 	oamInitGfxSet(&gameLogoSprite, 
 		(&gameLogoSprite_end-&gameLogoSprite), 
 		&gameLogoPalette, 
@@ -21,47 +17,57 @@ int main(void) {
 		OBJ_SIZE8_L32);
 
 	// Line 1 Part 1
-	oamSet(0, 0, 0, 3, 0, 0, 0, 0);
+	oamSet(0, xspr, yspr, 3, 0, 0, 0, 0);
 	oamSetEx(0, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4, 32, 0, 3, 0, 0, 4, 0);
+	oamSet(4, xspr + 32, yspr, 3, 0, 0, 4, 0);
 	oamSetEx(4, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*2, 32*2, 0, 3, 0, 0, 4*2, 0);
+	oamSet(4*2, xspr + 32*2, yspr, 3, 0, 0, 4*2, 0);
 	oamSetEx(4*2, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*3, 32*3, 0, 3, 0, 0, 4*3, 0);
+	oamSet(4*3, xspr + 32*3, yspr, 3, 0, 0, 4*3, 0);
 	oamSetEx(4*3, OBJ_LARGE, OBJ_SHOW);
 
 	// Line 1 Part 2
-	oamSet(4*4, 32*4, 0, 3, 0, 0, 64, 0);
+	oamSet(4*4, xspr + 32*4, yspr, 3, 0, 0, 64, 0);
 	oamSetEx(4*4, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*5, 32*5, 0, 3, 0, 0, 64 + 4, 0);
+	oamSet(4*5, xspr + 32*5, yspr, 3, 0, 0, 64 + 4, 0);
 	oamSetEx(4*5, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*6, 32*6, 0, 3, 0, 0, 64 + 4*2, 0);
+	oamSet(4*6, xspr + 32*6, yspr, 3, 0, 0, 64 + 4*2, 0);
 	oamSetEx(4*6, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*7, 32*7, 0, 3, 0, 0, 64 + 4*3, 0);
+	oamSet(4*7, xspr + 32*7, yspr, 3, 0, 0, 64 + 4*3, 0);
 	oamSetEx(4*7, OBJ_LARGE, OBJ_SHOW);
 
 	// Line 2 Part 1
-	oamSet(4*8, 0, 32, 3, 0, 0, 64*2 + 4*0, 0);
+	oamSet(4*8, xspr, yspr + 32, 3, 0, 0, 64*2 + 4*0, 0);
 	oamSetEx(4*8, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*9, 32, 32, 3, 0, 0, 64*2 + 4*1, 0);
+	oamSet(4*9, xspr + 32, yspr + 32, 3, 0, 0, 64*2 + 4*1, 0);
 	oamSetEx(4*9, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*10, 64, 32, 3, 0, 0, 64*2 + 4*2, 0);
+	oamSet(4*10, xspr + 64, yspr + 32, 3, 0, 0, 64*2 + 4*2, 0);
 	oamSetEx(4*10, OBJ_LARGE, OBJ_SHOW);
 
-	oamSet(4*11, 168, 32, 3, 0, 0, 64*2 + 4*3, 0);
+	oamSet(4*11, xspr + 168, yspr + 32, 3, 0, 0, 64*2 + 4*3, 0);
 	oamSetEx(4*11, OBJ_LARGE, OBJ_SHOW);
 
 	// Line 2 Part 2
-	oamSet(4*12, 200, 32, 3, 0, 0, 64*3 + 4*0, 0);
+	oamSet(4*12, xspr + 200, yspr + 32, 3, 0, 0, 64*3 + 4*0, 0);
 	oamSetEx(4*12, OBJ_LARGE, OBJ_SHOW);
+}
+
+int main(void) {
+    // Initialize SNES 
+	consoleInit();
+
+	// Now Put in 256 color mode
+	setMode(BG_MODE3, 0);
+
+	loadGameLogo(14, 102);
 
 	setScreenOn();
     
